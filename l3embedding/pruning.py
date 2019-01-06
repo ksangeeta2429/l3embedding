8b7f9245e4447b68c590de03a8ac00d228ebdf2b
ksangeeta2429 <kumari.14@osu.edu>
2019-01-06 01:34:16 -0500

    Adding output files

--- l3embedding/pruning.py
+++ l3embedding/pruning.py
@@ -31,11 +31,11 @@ from tensorflow.python.framework import dtypes
 ##########
 # Step 1
 # Pseudo-code
-# Construct the l3 model and load the weights from trained cnn_l3_melspec1
+# Construct the l3 model and load the weights from trained cnn_l3_melspec2
 # Version 1
 # Run a loop for different sparsity values [30, 40, 50, 60, 70] for each CONV block and test the performance
 # Version 2
-# Run a loop for different sparsity values [30, 40, 50, 60, 70] for each CONV layer and test the performance
+# Run a loop for different sparsity values [30, 40, 50, 60, 70, 80, 85, 90, 95] for each CONV layer and test the performance
 ##########
 
 ##########
@@ -154,6 +154,7 @@ def calculate_threshold(weights, ratio):
 
         
 def sparsify_layer(model, sparsity_dict):
+    masks = {}
     for layer in model.layers:
         if ('conv_' in layer.name and sparsity_dict[layer.name]):
             target_weights = np.empty_like(layer.get_weights())
@@ -161,14 +162,14 @@ def sparsify_layer(model, sparsity_dict):
             threshold = calculate_threshold(weights, sparsity_dict[layer.name])
             mask      = K.cast(K.greater(K.abs(weights), threshold), dtypes.float32)
 
+            masks[layer.name] = mask
             new_weights = weights * K.eval(mask)
             target_weights[0] = new_weights
             
             target_weights[1] = layer.get_weights()[1]
             layer.set_weights(target_weights)
-            #print(new_weights)
                         
-    return model
+    return model, masks
 
 def sparsify_block(model, sparsity_dict):
     conv_blocks = 4
@@ -245,6 +246,223 @@ def test(model, validation_data_dir, learning_rate=1e-4, validation_epoch_size=1
     
     return score
 
+
+def train_withoutKD(model, train_data_dir, validation_data_dir, output_dir = '/scratch/sk7898/pruning_without_kd_output',\
+                    num_epochs=300, train_epoch_size=4096, validation_epoch_size=1024, train_batch_size=64,\
+                    validation_batch_size=64, model_type = 'cnn_L3_melspec2', random_state=20180216, 
+                    learning_rate=0.00001, verbose=True, checkpoint_interval=10, gpus=1):
+
+# Form model ID
+    data_subset_name = os.path.basename(train_data_dir)
+    data_subset_name = data_subset_name[:data_subset_name.rindex('_')]
+    model_id = os.path.join(data_subset_name, model_type)
+    model_dir = os.path.join(output_dir, 'embedding', model_id, datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    
+    if not os.path.isdir(model_dir):
+        os.makedirs(model_dir)
+
+    param_dict = {
+          'train_data_dir': train_data_dir,
+          'validation_data_dir': validation_data_dir,
+          'model_id': model_id,
+          'output_dir': output_dir,
+          'num_epochs': num_epochs,
+          'train_epoch_size': train_epoch_size,
+          'validation_epoch_size': validation_epoch_size,
+          'train_batch_size': train_batch_size,
+          'validation_batch_size': validation_batch_size,
+          'model_type': model_type,
+          'random_state': random_state,
+          'learning_rate': learning_rate,
+          'verbose': verbose
+    }
+    
+    param_dict['model_dir'] = model_dir
+    train_config_path = os.path.join(model_dir, 'config.json')
+    with open(train_config_path, 'w') as fd:
+        json.dump(param_dict, fd, indent=2)
+
+    param_dict.update({
+          'latest_epoch': '-',
+          'latest_train_loss': '-',
+          'latest_validation_loss': '-',
+          'latest_train_acc': '-',
+          'latest_validation_acc': '-',
+          'best_train_loss': '-',
+          'best_validation_loss': '-',
+          'best_train_acc': '-',
+          'best_validation_acc': '-',
+    })
+
+    train_gen = data_generator(train_data_dir,
+                               batch_size=train_batch_size,
+                               random_state=random_state,
+                               start_batch_idx=None)
+
+    train_gen = pescador.maps.keras_tuples(train_gen,
+                                           ['video', 'audio'],
+                                           'label')
+
+    val_gen = single_epoch_data_generator(validation_data_dir,
+                                          validation_epoch_size,
+                                          batch_size=validation_batch_size,
+                                          random_state=random_state)
+
+    val_gen = pescador.maps.keras_tuples(val_gen,
+                                         ['video', 'audio'],
+                                         'label')
+
+    loss = 'categorical_crossentropy'
+    metrics = ['accuracy']
+    model.compile(Adam(lr=learning_rate),
+              loss=loss,
+              metrics=metrics)
+
+    history = model.fit_generator(train_gen, train_epoch_size, num_epochs,
+                                    validation_data=val_gen,
+                                    validation_steps=validation_epoch_size,
+                                    verbose=1)
+
+    print(history)
+    return model
+
+def train_withKD(student, train_data_dir, validation_data_dir, weight_path, output_dir = '/scratch/sk7898/pruning_with_kd_output', num_epochs=300,\
+                 train_epoch_size=4096, validation_epoch_size=1024, train_batch_size=64, validation_batch_size=64, model_type = 'cnn_L3_melspec2',\
+                 random_state=20180216, learning_rate=0.00001, verbose=True, checkpoint_interval=10, gpus=1):
+    
+    # Form model ID
+    data_subset_name = os.path.basename(train_data_dir)
+    data_subset_name = data_subset_name[:data_subset_name.rindex('_')]
+    model_id = os.path.join(data_subset_name, model_type)
+    model_dir = os.path.join(output_dir, 'embedding', model_id, datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    
+    if not os.path.isdir(model_dir):
+        os.makedirs(model_dir)
+
+    param_dict = {
+          'train_data_dir': train_data_dir,
+          'validation_data_dir': validation_data_dir,
+          'model_id': model_id,
+          'output_dir': output_dir,
+          'num_epochs': num_epochs,
+          'train_epoch_size': train_epoch_size,
+          'validation_epoch_size': validation_epoch_size,
+          'train_batch_size': train_batch_size,
+          'validation_batch_size': validation_batch_size,
+          'model_type': model_type,
+          'random_state': random_state,
+          'learning_rate': learning_rate,
+          'verbose': verbose
+    }
+    
+    param_dict['model_dir'] = model_dir
+    train_config_path = os.path.join(model_dir, 'config.json')
+    with open(train_config_path, 'w') as fd:
+        json.dump(param_dict, fd, indent=2)
+
+    param_dict.update({
+          'latest_epoch': '-',
+          'latest_train_loss': '-',
+          'latest_validation_loss': '-',
+          'latest_train_acc': '-',
+          'latest_validation_acc': '-',
+          'best_train_loss': '-',
+          'best_validation_loss': '-',
+          'best_train_acc': '-',
+          'best_validation_acc': '-',
+    })
+
+    student, x_a, y_a = load_student_audio_model()
+
+    optimizer = keras.optimizers.SGD(lr=learning_rate, momentum=0.9, nesterov=True)
+
+    student.compile(Adam(lr=learning_rate),
+                    loss='mean_squared_error',
+                    metrics=['mae'])
+
+    # Save the model
+    model_spec_path = os.path.join(model_dir, 'model_spec.pkl')
+    model_spec = keras.utils.serialize_keras_object(student)
+    with open(model_spec_path, 'wb') as fd:
+        pickle.dump(model_spec, fd)
+    model_json_path = os.path.join(model_dir, 'model.json')
+    model_json = student.to_json()
+    with open(model_json_path, 'w') as fd:
+        json.dump(model_json, fd, indent=2)
+
+    latest_weight_path = os.path.join(model_dir, 'model_latest.h5')
+    best_valid_loss_weight_path = os.path.join(model_dir, 'model_best_valid_loss.h5')
+    checkpoint_weight_path = os.path.join(model_dir, 'model_checkpoint.{epoch:02d}.h5')
+
+    # Set up callbacks
+    cb = []
+
+    best_val_loss_cb = keras.callbacks.ModelCheckpoint(best_valid_loss_weight_path,
+                                                       save_weights_only=True,
+                                                       save_best_only=True,
+                                                       verbose=1,
+                                                       monitor='val_loss')
+    #if continue_model_dir is not None:
+    #    best_val_loss_cb.best = last_val_loss
+    cb.append(best_val_loss_cb)
+
+    checkpoint_cb = keras.callbacks.ModelCheckpoint(checkpoint_weight_path,
+                                                    save_weights_only=True,
+                                                    period=checkpoint_interval)
+    #if continue_model_dir is not None:
+    #    checkpoint_cb.epochs_since_last_save = (last_epoch_idx + 1) % checkpoint_interval
+    cb.append(checkpoint_cb)
+
+    earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-4, patience=10)
+    reduceLR = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5)
+    
+    cb.append(earlyStopping)
+    cb.append(reduceLR)
+
+    #LOGGER.info('Setting up train data generator...')
+    #if continue_model_dir is not None:
+    #    train_start_batch_idx = train_epoch_size * (last_epoch_idx + 1)
+    #else:
+    train_start_batch_idx = None
+
+    train_gen = data_generator(train_data_dir,
+                               batch_size=train_batch_size,
+                               random_state=random_state,
+                               start_batch_idx=train_start_batch_idx)
+
+    train_gen = pescador.maps.keras_tuples(train_gen,
+                                           'audio',
+                                           'label')
+
+    val_gen = single_epoch_data_generator(validation_data_dir,
+                                          validation_epoch_size,
+                                          batch_size=validation_batch_size,
+                                          random_state=random_state)
+
+    val_gen = pescador.maps.keras_tuples(val_gen,
+                                         'audio',
+                                         'label')
+
+    history = student.fit_generator(train_gen, train_epoch_size, num_epochs,
+                                    validation_data=val_gen,
+                                    validation_steps=validation_epoch_size,
+                                    callbacks=cb,
+                                    verbose=1)
+
+    # Save history
+    with open(os.path.join(model_dir, 'history.pkl'), 'wb') as fd:
+        pickle.dump(history.history, fd)
+
+    return model
+
+'''
+def finetune(l3_model, sparsified_audio_model, masks, train_data_dir, validation_data_dir, kd=False):
+    if kd:
+        #Fine-tuning with knowledge distillation
+    else:
+        #Fine-tuning without knowledge distillation
+'''
+
 def zero_check(sparsity_list):
     for val in sparsity_list:
         if(val):
@@ -257,16 +475,22 @@ def printList(sparsity_list):
         printStr += str(val) + ", "
     print(printStr)
 
-def pruning(weight_path, validation_dir, blockwise=False, layerwise=False):
+def pruning(weight_path, validation_dir, blockwise=False, layerwise=False, per_layer=False):
     conv_blocks = 4
     
-    sparsity_values = [70., 80., 90.]
+    sparsity_values = [70., 80., 85., 90., 95.]
     sparsity_blks = [0, 0, 0, 0]
-    #sparsity_layers = [0, 0, 0, 0, 0, 0, 0, 0]
-    sparsity_layers = [[0, 40., 40., 50., 30., 50., 50., 60.],\
-                       [0, 40., 50., 50., 30., 60., 60., 60.],\
-                       [0, 40., 50., 60., 30., 60., 60., 60.]]
+    if per_layer:
+        sparsity_layers = [0, 0, 0, 0, 0, 0, 0, 0]
+    else:
+        sparsity_layers = [[0, 30., 40., 50., 30., 50., 50., 60.],\
+                           [0, 40., 50., 60., 40., 70., 70., 80.]]
 
+        '''
+                           [0, 60., 60., 70., 50., 70., 70., 80.],\
+                           [0, 70., 70., 75., 60., 80., 80., 85.],\
+                           [0, 80., 80., 85., 40., 85., 85., 95.]]
+        '''
     if(blockwise and zero_check(sparsity_blks)):
         print("Block-wise Pruning")
         print("*********************************************************************")
@@ -284,14 +508,14 @@ def pruning(weight_path, validation_dir, blockwise=False, layerwise=False):
                 print('Loss: {0} Accuracy: {1}'.format(score[0], score[1]))     
                 print('---------------------------------------------------------------')
     
-    if(layerwise and zero_check(sparsity_layers[0])):
+    if(layerwise and per_layer):
         print("Layer-wise Pruning")
         print("**********************************************************************")
         for sparsity in sparsity_values:
             for layerid in range(conv_blocks*2):
                 model, audio_model = load_audio_model_for_pruning(weight_path)
                 sparsity_vals = get_sparsity_layers(layerid, sparsity, sparsity_layers)
-                sparsified_model = sparsify_layer(audio_model, sparsity_vals)
+                sparsified_model, masks = sparsify_layer(audio_model, sparsity_vals)
 
                 model.get_layer('audio_model').set_weights(sparsified_model.get_weights())
 
@@ -300,21 +524,23 @@ def pruning(weight_path, validation_dir, blockwise=False, layerwise=False):
                 print('Loss: {0} Accuracy: {1}'.format(score[0], score[1]))
                 print('----------------------------------------------------------------')
     
-    if(layerwise and not zero_check(sparsity_layers[0])):
+    if(layerwise and not per_layer):
         print("Specific Pruning Values")
         print("**********************************************************************")
         for sparsity in sparsity_layers: 
             model, audio_model = load_audio_model_for_pruning(weight_path)
+            print(audio_model.summary())
             sparsity_vals = get_sparsity_layers(None, None, sparsity)
-            sparsified_model = sparsify_layer(audio_model, sparsity_vals)
+            sparsified_model, masks = sparsify_layer(audio_model, sparsity_vals)
 
             model.get_layer('audio_model').set_weights(sparsified_model.get_weights())
             score = test(model, validation_dir)
             printList(sparsity)
             print('Loss: {0} Accuracy: {1}'.format(score[0], score[1]))
             print('----------------------------------------------------------------')
+            
 
 weight_path = '/home/sk7898/l3embedding/models/cnn_l3_melspec2_recent/model_best_valid_accuracy.h5'
 validation_dir = '/beegfs/work/AudioSetSamples/music_valid'
 #validation_dir = '/beegfs/work/AudioSetSamples_environmental/environmental_valid'
-pruning(weight_path, validation_dir, blockwise=False, layerwise=True)
+pruning(weight_path, validation_dir, blockwise=False, layerwise=True, per_layer=False)
