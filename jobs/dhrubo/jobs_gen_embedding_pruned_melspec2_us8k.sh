d24a76f3ead335c9df9ecbd9b473fc322a43f3b8
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-01-11 01:20:50 -0500

    Adding embedding extraction for 3 more pruned models

--- jobs/dhrubo/jobs_gen_embedding_pruned_melspec2_us8k.sh
+++ jobs/dhrubo/jobs_gen_embedding_pruned_melspec2_us8k.sh
@@ -5,3 +5,9 @@ sleep 1
 sbatch --export=filename=pruned_audio_0.75373.h5 gen_embedding_pruned_melspec2_us8k.sbatch
 sleep 1
 sbatch --export=filename=pruned_audio_0.77128.h5 gen_embedding_pruned_melspec2_us8k.sbatch
+sleep 1
+sbatch --export=filename=pruned_audio_0.51833.h5 gen_embedding_pruned_melspec2_us8k.sbatch
+sleep 1
+sbatch --export=filename=pruned_audio_0.571075.h5 gen_embedding_pruned_melspec2_us8k.sbatch
+sleep 1
+sbatch --export=filename=pruned_audio_0.685913.h5 gen_embedding_pruned_melspec2_us8k.sbatch
