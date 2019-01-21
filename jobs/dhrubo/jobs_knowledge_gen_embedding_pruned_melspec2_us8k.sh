72b44caed4d56802232a8cdd21d1c5c859c1721d
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-01-21 15:33:38 -0500

    Fixing filename

--- jobs/dhrubo/jobs_knowledge_gen_embedding_pruned_melspec2_us8k.sh
+++ jobs/dhrubo/jobs_knowledge_gen_embedding_pruned_melspec2_us8k.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sbatch --export=filename=/scratch/dr2915/l3pruning/embedding/pruning_kd/music/cnn_L3_melspec2/20190117222133 gen_embedding_pruned_melspec2_us8k.sbatch
+sbatch --export=filename=/scratch/dr2915/l3pruning/embedding/pruning_kd/music/cnn_L3_melspec2/20190117222133/model_best_valid_loss.h5 gen_embedding_pruned_melspec2_us8k.sbatch
 sleep 1
