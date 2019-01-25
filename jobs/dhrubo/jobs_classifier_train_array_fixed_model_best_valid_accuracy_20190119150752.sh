ee15a0ddc21b02c79c33bdc20c03cce49b17264c
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2019-01-25 14:26:54 -0500

    Adding script to generate classifier jobs for fixed modelsa

---
+++ jobs/dhrubo/jobs_classifier_train_array_fixed_model_best_valid_accuracy_20190119150752.sh
@@ -0,0 +1,20 @@
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 1
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 2
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 3
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 4
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 5
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 6
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 7
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 8
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 9
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190119150752 10
+sleep 1
