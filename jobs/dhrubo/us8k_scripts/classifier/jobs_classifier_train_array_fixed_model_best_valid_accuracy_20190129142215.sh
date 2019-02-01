acf4cb05a90f9d9828d55af05cf4ffb03acaee8b
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2019-02-01 14:58:04 -0500

    Adding new classifier jobs

---
+++ jobs/dhrubo/us8k_scripts/classifier/jobs_classifier_train_array_fixed_model_best_valid_accuracy_20190129142215.sh
@@ -0,0 +1,20 @@
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 1
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 2
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 3
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 4
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 5
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 6
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 7
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 8
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 9
+sleep 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/fixed/model_best_valid_accuracy_20190129142215 10
+sleep 1
