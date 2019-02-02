cb6828e997d6be7851e375165bc6b85cb862a06e
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2019-02-02 12:13:45 -0500

    Adding many jobs

---
+++ jobs/dhrubo/esc50/classifier/pre-finetune/jobs_classifier_train_array_pruned_audio_0.4989776611328125.sh
@@ -0,0 +1,10 @@
+sbatch classifier-train-array-esc50.sbatch /scratch/dr2915/embeddings/features/esc50/l3comp/short/pruned_model/pruned_audio_0.4989776611328125 1
+sleep 1
+sbatch classifier-train-array-esc50.sbatch /scratch/dr2915/embeddings/features/esc50/l3comp/short/pruned_model/pruned_audio_0.4989776611328125 2
+sleep 1
+sbatch classifier-train-array-esc50.sbatch /scratch/dr2915/embeddings/features/esc50/l3comp/short/pruned_model/pruned_audio_0.4989776611328125 3
+sleep 1
+sbatch classifier-train-array-esc50.sbatch /scratch/dr2915/embeddings/features/esc50/l3comp/short/pruned_model/pruned_audio_0.4989776611328125 4
+sleep 1
+sbatch classifier-train-array-esc50.sbatch /scratch/dr2915/embeddings/features/esc50/l3comp/short/pruned_model/pruned_audio_0.4989776611328125 5
+sleep 1
--- jobs/dhrubo/us8k_scripts/classifier/pre-finetune/jobs_classifier_train_array_pruned_audio_0.77128.sh
+++ jobs/dhrubo/us8k_scripts/classifier/pre-finetune/jobs_classifier_train_array_pruned_audio_0.77128.sh
@@ -1,23 +1,20 @@
-#!/usr/bin/env bash
-
-#/scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/
-
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 1
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 1
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 2
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 2
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 3
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 3
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 4
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 4
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 5
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 5
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 6
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 6
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 7
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 7
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 8
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 8
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 9
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 9
 sleep 1
-sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128/ 10
+sbatch classifier-train-array-us8k.sbatch /scratch/dr2915/embeddings/features/us8k/l3comp/short/pruned_model/pruned_audio_0.77128 10
+sleep 1
