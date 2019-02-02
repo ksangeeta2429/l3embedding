61553233a155355d49052b8b522617e7e7a6a5c7
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-02-01 23:27:29 -0500

    Script to create untuned US8K classifier jobs

---
+++ jobs/dhrubo/us8k_scripts/classifier/pre-finetune/CREATE_UNTUNED_EMBEDDING_JOBS_us8k.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+features=( `find $SCRATCH/embeddings/features/us8k/l3comp/short/ -maxdepth 1 -mindepth 1 -type d` )
+
+for f in ${features[@]}; do
+	filename=`basename $f`
+	outname=jobs_classifier_train_array_fixed_$filename.sh
+	rm -f $outname
+	for i in `seq 1 10`; do
+		echo sbatch classifier-train-array-us8k.sbatch $f $i >> $outname
+		echo sleep 1 >> $outname
+	done
+done
