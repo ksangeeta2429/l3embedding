1b7945ca982586a2efa72745cbfa1895a7580352
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-02-26 23:48:24 -0500

    Adding more required stuff

--- load_embedding_pi.py
+++ load_embedding_pi.py
@@ -18,22 +18,34 @@ from tensorflow.python.ops import nn_ops
 from tensorflow.python.keras.utils import conv_utils
 from tensorflow.python.framework import dtypes
 from l3embedding.model import load_embedding
+from data.usc.us8k import generate_us8k_fold_data
 
 import time
 
+# Load embedding
 weights_path = 'cnn_l3_melspec2_recent/model_best_valid_accuracy.h5'
 model_type = 'cnn_L3_melspec2'
 embedding_type = 'audio'
 pooling_type = 'short'
 
-# Load embedding
+l3embedding_model = load_embedding(weights_path, model_type, embedding_type, pooling_type)
 
+# Featurization
+fold_num = 5
+features = 'l3'
+metadata_path = 'UrbanSound8K/metadata/UrbanSound8K.csv'
+data_dir = 'UrbanSound8K/audio'
+dataset_output_dir = 'embeddings'
+random_state = ''
+hop_size = 0.1
 
 print('Starting to profile...')
 start = time.time()
 
-# Featurization
-embedding = load_embedding(weights_path, model_type, embedding_type, pooling_type)
+generate_us8k_fold_data(metadata_path, data_dir, fold_num-1, dataset_output_dir,
+                                    l3embedding_model=l3embedding_model,
+                                    features=features, hop_size=hop_size)
+
 done = time.time()
 elapsed = done - start
 print(elapsed)
