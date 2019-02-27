dde3fedd692c6c181ad9ff2a5b803847bb9aa405
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-02-27 16:19:22 -0500

    Adding simpler processing for US8K data

--- data/usc/us8k.py
+++ data/usc/us8k.py
@@ -39,112 +39,12 @@ def load_us8k_metadata(path):
 
     return metadata
 
-
-
-def generate_us8k_folds(metadata_path, data_dir, output_dir, l3embedding_model=None,
-                        features='l3', random_state=12345678, **feature_args):
-    """
-    Generate all of the data for each fold
-
-    Args:
-        metadata_path: Path to metadata file
-                       (Type: str)
-
-        data_dir: Path to data directory
-                  (Type: str)
-
-        output_dir: Path to output directory where fold data will be stored
-                    (Type: str)
-
-    Keyword Args:
-        l3embedding_model: L3 embedding model, used if L3 features are used
-                           (Type: keras.engine.training.Model or None)
-
-        features: Type of features to be computed
-                  (Type: str)
-
-    """
-    LOGGER.info('Generating all folds.')
-    metadata = load_us8k_metadata(metadata_path)
-
-    for fold_idx in range(NUM_FOLDS):
-        generate_us8k_fold_data(metadata, data_dir, fold_idx, output_dir,
-                                l3embedding_model=l3embedding_model,
-                                features=features, random_state=random_state,
-                                **feature_args)
-
-
-def generate_us8k_fold_data(metadata, data_dir, fold_idx, output_dir, l3embedding_model=None,
-                            features='l3', random_state=12345678, **feature_args):
-    """
-    Generate all of the data for a specific fold
-
-    Args:
-        metadata: List of metadata dictionaries, or a path to a metadata file to be loaded
-                  (Type: list[dict[str,*]] or str)
-
-        data_dir: Path to data directory
-                  (Type: str)
-
-        fold_idx: Index of fold to load
-                  (Type: int)
-
-        output_dir: Path to output directory where fold data will be stored
-                    (Type: str)
-
-    Keyword Args:
-        l3embedding_model: L3 embedding model, used if L3 features are used
-                           (Type: keras.engine.training.Model or None)
-
-        features: Type of features to be computed
-                  (Type: str)
-
-    """
-
-    if type(metadata) == str:
-        metadata = load_us8k_metadata(metadata)
-
-    # Set random seed
-    random_state = random_state + fold_idx
-    random.seed(random_state)
-    np.random.seed(random_state)
-
-    audio_fold_dir = os.path.join(data_dir, "fold{}".format(fold_idx+1))
-
-    # Create fold directory if it does not exist
-    output_fold_dir = os.path.join(output_dir, "fold{}".format(fold_idx+1))
-    if not os.path.isdir(output_fold_dir):
-        os.makedirs(output_fold_dir)
-
-    LOGGER.info('Generating fold {} in {}'.format(fold_idx+1, output_fold_dir))
-
-    num_files = len(metadata[fold_idx])
-
-    for idx, (fname, example_metadata) in enumerate(metadata[fold_idx].items()):
-        desc = '({}/{}) Processed {} -'.format(idx+1, num_files, fname)
-        with LogTimer(LOGGER, desc, log_level=logging.DEBUG):
-            # TODO: Make sure glob doesn't catch things with numbers afterwards
-            variants = [x for x in glob.glob(os.path.join(audio_fold_dir,
-                '**', os.path.splitext(fname)[0] + '[!0-9]*[wm][ap][v3]'), recursive=True)
-                if os.path.isfile(x) and not x.endswith('.jams')]
-            num_variants = len(variants)
-            for var_idx, var_path in enumerate(variants):
-                audio_dir = os.path.dirname(var_path)
-                var_fname = os.path.basename(var_path)
-                desc = '\t({}/{}) Variants {} -'.format(var_idx+1, num_variants, var_fname)
-                with LogTimer(LOGGER, desc, log_level=logging.DEBUG):
-                    generate_us8k_file_data(var_fname, example_metadata, audio_dir,
-                                            output_fold_dir, features,
-                                            l3embedding_model, **feature_args)
-
-
-def generate_us8k_file_data(fname, example_metadata, audio_fold_dir,
-                            output_fold_dir, features,
+def generate_us8k_file_data(fname, example_metadata, audio_fold_dir, features,
                             l3embedding_model, **feature_args):
     audio_path = os.path.join(audio_fold_dir, fname)
 
     basename, _ = os.path.splitext(fname)
-    output_path = os.path.join(output_fold_dir, basename + '.npz')
+    #output_path = os.path.join(output_fold_dir, basename + '.npz')
 
     #if os.path.exists(output_path):
     #    LOGGER.info('File {} already exists'.format(output_path))
@@ -163,9 +63,10 @@ def generate_us8k_file_data(fname, example_metadata, audio_fold_dir,
 
     class_label = example_metadata['classID']
     y = class_label
+    return X, y
 
-    np.savez_compressed(output_path, X=X, y=y)
+    #np.savez_compressed(output_path, X=X, y=y)
 
-    return output_path, 'success'
+    #return output_path, 'success'
 
 
--- load_embedding_pi.py
+++ load_embedding_pi.py
@@ -1,5 +1,9 @@
 from l3embedding.model import load_embedding
-from data.usc.us8k import generate_us8k_fold_data
+from data.usc.us8k import generate_us8k_file_data, load_us8k_metadata
+import random
+import numpy as np
+import os
+import glob
 
 # Load embedding
 weights_path = 'models/cnn_l3_melspec2_recent/model_best_valid_accuracy.h5'
@@ -11,16 +15,38 @@ print('Loading embedding...')
 l3embedding_model = load_embedding(weights_path, model_type, embedding_type, pooling_type)
 
 # Featurization
-fold_num = 5
+fold_idx = 5
 features = 'l3'
 metadata_path = 'UrbanSound8K/metadata/UrbanSound8K.csv'
 data_dir = 'UrbanSound8K/audio'
 dataset_output_dir = 'embeddings'
 hop_size = 0.1
 
-print('Computing features...')
-for i in range(100):
-    generate_us8k_fold_data(metadata_path, data_dir, fold_num-1, dataset_output_dir,
-                                    l3embedding_model=l3embedding_model,
-                                    features=features, hop_size=hop_size)
+metadata = load_us8k_metadata(metadata_path)
+
+# 0-indexed fold id
+fold_idx = fold_idx - 1
+
+# Set random seed
+random_state = 12345678
+random_state = random_state + fold_idx
+random.seed(random_state)
+np.random.seed(random_state)
+
+# Audio fold directory
+audio_fold_dir = os.path.join(data_dir, "fold{}".format(fold_idx+1))
+
+for idx, (fname, example_metadata) in enumerate(metadata[fold_idx].items()):
+    variants = [x for x in glob.glob(os.path.join(audio_fold_dir,
+                                                  '**', os.path.splitext(fname)[0] + '[!0-9]*[wm][ap][v3]'),
+                                     recursive=True) if os.path.isfile(x) and not x.endswith('.jams')]
+
+    for var_idx, var_path in enumerate(variants):
+        audio_dir = os.path.dirname(var_path)
+        var_fname = os.path.basename(var_path)
+        print('Computing features...')
+        # Keep iterating
+        for i in range(100):
+            X, y = generate_us8k_file_data(var_fname, example_metadata, audio_dir, features=features,
+                                l3embedding_model=l3embedding_model, hop_size=hop_size)
 
