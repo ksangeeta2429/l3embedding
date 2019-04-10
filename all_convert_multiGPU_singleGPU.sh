7d2841cf07e03a75345745f358e8e42776d639f3
ksangeeta2429 <kumari.14@osu.edu>
2019-04-10 14:03:20 -0400

    SCRIPT changes

---
+++ all_convert_multiGPU_singleGPU.sh
@@ -0,0 +1,19 @@
+sh convert_multiGPU_singleGPU.sbatch /scratch/sk7898/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190303194756 48000 256 484 2048
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/sk7898/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190303210054 48000 256 968 2048
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/dr2915/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190304024942 48000 64 968 2048
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/mc6591/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190304160730 48000 128 968 2048
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/mc6591/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190304160731 48000 64 242 2048
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/dr2915/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190304165251 48000 64 1936 2048
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/cm3580/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190305211344 16000 64 484 1024
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/cm3580/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190306112819 16000 64 968 1024
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/sk7898/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190310204758 48000 256 1936 2048
+sleep 1
+sh convert_multiGPU_singleGPU.sbatch /scratch/mc6591/l3pruning/reduced_input/embedding/music/cnn_L3_melspec2/20190311170637 48000 128 1936 2048
