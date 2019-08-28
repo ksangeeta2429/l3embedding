d832bef975bbbaf5781df2705951fe04495ed716
ksangeeta2429 <kumari.14@osu.edu>
2019-08-28 02:13:32 -0400

    keras in diff function for multiprocessing

--- l3embedding/save_approx_embedding.py
+++ l3embedding/save_approx_embedding.py
@@ -1,27 +1,48 @@
 import os
 import random
 import numpy as np
-import keras
-import tensorflow as tf
 import h5py
-from l3embedding.model import *
 from l3embedding.audio import pcm2float
 import umap
 from sklearn.manifold import TSNE
 import multiprocessing
 from joblib import Parallel, delayed
-from log import init_console_logger
-
-LOGGER = logging.getLogger('embedding_generator')
-LOGGER.setLevel(logging.DEBUG)
-
-graph = tf.get_default_graph()
-weight_path = 'models/cnn_l3_melspec2_recent/model_best_valid_accuracy.h5'
-audio_model = load_embedding(weight_path, model_type = 'cnn_L3_melspec2', embedding_type = 'audio', \
-                             pooling_type = 'short', kd_model=False, tgt_num_gpus = 1)
 
 
-def get_embedding(data, method, emb_len=None, neighbors=10, metric='euclidean', \
+def get_teacher_embedding(audio_batch):
+    import keras
+    import tensorflow as tf
+    from kapre.time_frequency import Melspectrogram
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    
+    try:
+        with tf.Graph().as_default(), tf.Session().as_default(), tf.device('/gpu:0'):
+            weight_path = '/scratch/sk7898/l3pruning/embedding/fixed/reduced_input/l3_full_original_48000_256_242_2048.h5'
+            model = keras.models.load_model(weight_path, custom_objects={'Melspectrogram': Melspectrogram}) 
+            embeddings = model.get_layer('audio_model').predict(audio_batch)
+            return embeddings
+
+    except GeneratorExit:
+        pass
+
+
+def write_to_h5(paths, batch, batch_size):
+    n_files = int(batch_size/1024)
+    start_idx = 0
+    
+    for path in paths:
+        end_idx = start_idx + 1024
+            
+        with h5py.File(path, 'a') as f:
+            for key in batch.keys():
+                if key in f.keys():
+                    continue
+                f.create_dataset(key, data=batch[key][start_idx:end_idx], compression='gzip')
+            f.close()
+        start_idx = end_idx
+
+
+def get_reduced_embedding(data, method, emb_len=None, neighbors=10, metric='euclidean', \
                 min_dist=0.3, iterations=500):
     
     if len(data) == 0:
@@ -50,7 +71,7 @@ def get_blob_keys(method, batch_size, emb_len, neighbors_list=None, metric_list=
             raise ValueError('Either neighbor_list or metric_list or min_dist_list is missing')
         
         [blob_keys.append('umap_batch_' + str(batch_size) + \
-                          '/len_' + str(emb_len) + \
+                          '_len_' + str(emb_len) + \
                           '_k_' + str(neighbors) + \
                           '_metric_' + metric + \
                           '_dist|iter_' + str(min_dist)) \
@@ -61,7 +82,7 @@ def get_blob_keys(method, batch_size, emb_len, neighbors_list=None, metric_list=
             raise ValueError('Either neighbor_list or metric_list or tsne_iter_list is missing')
         
         [blob_keys.append('tsne_batch_' + str(batch_size) +\
-                          '/len_' + str(emb_len) + \
+                          '_len_' + str(emb_len) + \
                           '_batch_' + str(batch_size) + \
                           '_k_' + str(neighbors) + \
                           '_metric_' + metric + \
@@ -82,38 +103,30 @@ def embedding_generator(data_dir, output_dir, reduced_emb_len, approx_mode='umap
     
     if metric_list is None:
         metric_list = ['euclidean']
-        print('Training UMAP with default value of metric: euclidean')
 
     if approx_mode == 'umap' and min_dist_list is None:
         min_dist_list = [0.3]
-        print('Training UMAP with default value of min_dist: 0.3')
-
-    init_console_logger(LOGGER, verbose=True)        
+    
     random.seed(random_state)
     
     batch = None
-    global graph
-    global audio_model
+    embedding_out_paths = []
     curr_batch_size = 0
     batch_idx = 0
-    keys = ['audio']
-    blob_keys = get_blob_keys(approx_mode, batch_size, reduced_emb_len, neighbors_list=neighbors_list, \
-                              metric_list=metric_list, min_dist_list=min_dist_list, tsne_iter_list=tsne_iter_list)
-    LOGGER.info('Embedding Blob Keys: ', blob_keys)
 
-    for fname in os.listdir(data_dir):
-        LOGGER.info('Data filename: ', fname)
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
         
-        blob_embeddings = dict()
+    for fname in os.listdir(data_dir):
+        print('Data filename: {}'.format(fname))
+
         batch_path = os.path.join(data_dir, fname)
         blob_start_idx = 0
-        blob_end_idx = 0
 
         blob = h5py.File(batch_path, 'r')
         blob_size = len(blob['label'])
-
-        old_embeddings = np.zeros((blob_size, 512), dtype=np.float32)
-        embedding_out_path = os.path.join(output_dir, fname)
+        
+        embedding_out_paths.append(os.path.join(output_dir, fname))
 
         while blob_start_idx < blob_size:
             blob_end_idx = min(blob_start_idx + batch_size - curr_batch_size, blob_size)
@@ -122,76 +135,67 @@ def embedding_generator(data_dir, output_dir, reduced_emb_len, approx_mode='umap
             # the prior batches
             if start_batch_idx is None or batch_idx >= start_batch_idx:
                 if batch is None:
-                    batch = {k:blob[k][blob_start_idx:blob_end_idx]
-                             for k in keys}
+                    batch = {'audio':blob['audio'][blob_start_idx:blob_end_idx]}
                 else:
-                    for k in keys:
-                        batch[k] = np.concatenate([batch[k],
-                                                   blob[k][blob_start_idx:blob_end_idx]])
+                    batch['audio'] = np.concatenate([batch['audio'], blob['audio'][blob_start_idx:blob_end_idx]])
 
             curr_batch_size += blob_end_idx - blob_start_idx
-
+            blob_start_idx = blob_end_idx
+            
             if blob_end_idx == blob_size:
                 blob.close()
 
             if curr_batch_size == batch_size:
+                blob_embeddings = dict()
+                blob_keys = get_blob_keys(approx_mode, batch_size, reduced_emb_len, \
+                                          neighbors_list=neighbors_list, metric_list=metric_list, \
+                                          min_dist_list=min_dist_list, tsne_iter_list=tsne_iter_list)
+    
+                print('Embedding Blob Keys: {}'.format(blob_keys))
+        
                 # If we are starting from a particular batch, skip yielding all
                 # of the prior batches
                 if start_batch_idx is None or batch_idx >= start_batch_idx:
                     # Convert audio to float
                     batch['audio'] = pcm2float(batch['audio'], dtype='float32')
+                    
                     # Get the embedding layer output from the audio_model and flatten it to be treated as labels for the student audio model
-                    with graph.as_default():
-                        teacher_embedding = audio_model.predict(batch['audio'])
-
-                    old_embeddings[blob_start_idx:blob_end_idx, :] = teacher_embedding
+                    teacher_embedding = get_teacher_embedding(batch['audio'])
                     
                     if approx_mode == 'umap':
                         n_process = len(neighbors_list) * len(metric_list) * len(min_dist_list)
                         
-                        results = Parallel(n_jobs=-1)(delayed(get_embedding)(teacher_embedding, 'umap', \
-                                                                             emb_len=reduced_emb_len, \
-                                                                             neighbors=neighbors, \
-                                                                             metric=metric, \
-                                                                             min_dist=min_dist) \
+                        results = Parallel(n_jobs=-1)(delayed(get_reduced_embedding)(teacher_embedding, 'umap', \
+                                                                                     emb_len=reduced_emb_len, \
+                                                                                     neighbors=neighbors, \
+                                                                                     metric=metric, \
+                                                                                     min_dist=min_dist) \
                                           for neighbors in neighbors_list for metric in metric_list for min_dist in min_dist_list)
 
                     elif approx_mode == 'tsne':
                         n_process = len(neighbors_list) * len(metric_list) * len(tsne_iter_list)
                         
-                        results = Parallel(n_jobs=-1)(delayed(get_embedding)(teacher_embedding, 'tsne', \
-                                                                             emb_len=reduced_emb_len, \
-                                                                             neighbors=neighbors, \
-                                                                             metric=metric, \
-                                                                             iterations=iterations) \
+                        results = Parallel(n_jobs=-1)(delayed(get_reduced_embedding)(teacher_embedding, 'tsne', \
+                                                                                     emb_len=reduced_emb_len, \
+                                                                                     neighbors=neighbors, \
+                                                                                     metric=metric, \
+                                                                                     iterations=iterations) \
                                           for neighbors in neighbors_list for metric in metric_list for iterations in tsne_iter_list)
 
                         assert len(results) == n_process
                         
-                        for idx in range(len(results)):
-                            if blob_keys[idx] not in blob_embeddings.keys():    
-                                blob_embeddings[blob_keys[idx]] = np.zeros((blob_size, reduced_emb_len), dtype=np.float32)
-                                blob_embeddings[blob_keys[idx]][blob_start_idx:blob_end_idx,:] = results[idx]
-                            else:
-                                blob_embeddings[blob_keys[idx]][blob_start_idx:blob_end_idx,:] = results[idx]
-                                        
-                blob_start_idx = blob_end_idx
+                    for idx in range(len(results)):
+                        if blob_keys[idx] not in blob_embeddings.keys():    
+                            blob_embeddings[blob_keys[idx]] = np.zeros((blob_size, reduced_emb_len), dtype=np.float32)
+                            blob_embeddings[blob_keys[idx]] = results[idx]
+                        else:
+                            blob_embeddings[blob_keys[idx]] = results[idx]
+                      
+                        blob_embeddings['l3_embedding'] = teacher_embedding
+                        write_to_h5(embedding_out_paths, blob_embeddings, batch_size) 
+                        print('Batch completed!')
+                
                 batch_idx += 1
                 curr_batch_size = 0
-                batch = None
-                LOGGER.info('Batch completed!')
-
-        LOGGER.info('Saving embeddings in file!')
-        if os.path.exists(embedding_out_path):
-            mode = 'a' 
-        else:
-            mode = 'w'
-            
-        with h5py.File(embedding_out_path, mode) as f:
-            if 'embedding' not in f.keys():
-                f.create_dataset('embedding', data=old_embeddings) 
-            for key in blob_keys:
-                if key in f.keys():
-                    continue
-                f.create_dataset(key, data=blob_embeddings[key])
-            f.close()
+                batch = None 
+                embedding_out_paths = []
