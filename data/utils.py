4639a4a405fcd81f7928e1b7006a4a30e5ffdfac
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-10-02 17:56:08 -0400

    Adding utils to select subgroup of sonyc files Num. of datasets between 1400 and 1500

--- data/utils.py
+++ data/utils.py
@@ -1,6 +1,38 @@
 import csv
 from multiprocessing import Pool
+import glob
+import h5py
+import os
+import math
 
+def read_sonyc_num_files(csv_path):
+    csvread = csv.reader(open(csv_path, newline=''), delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    list_files = [row for row in csvread][0]
+    return len(list_files)
+
+
+def check_sonyc_openl3_points(feature_dir, out_path, verbose=True,
+                              min_num_datasets_per_file=0, max_num_datasets_per_file=math.inf):
+    num_pts = 0
+    num_sets = 0
+    files = glob.glob(os.path.join(feature_dir, '*/*.h5'))
+    valid_files=[]
+    for fname in files:
+        f = h5py.File(fname, 'r')
+        if min_num_datasets_per_file < f[list(f.keys())[0]].shape[0] < max_num_datasets_per_file:
+            valid_files += [fname]
+            num_sets += f[list(f.keys())[0]].shape[0]
+            if verbose:
+                print('File: {} Num. of datasets: {}'.format(fname, f[list(f.keys())[0]].shape[0]))
+            num_pts += f[list(f.keys())[0]].shape[0] * f[list(f.keys())[0]][0][1].shape[0]
+
+    print('Num files:', len(valid_files))
+    print('Num points:', num_pts)
+    print('Num datasets:', num_sets)
+
+    # Write valid files to csv
+    csvwrite = csv.writer(open(out_path, 'w', newline=''), delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    csvwrite.writerow(valid_files)
 
 def read_csv_as_dicts(path):
     items = []
@@ -31,3 +63,8 @@ def flatten_dict(dct, parent_key=None):
 
     return new_dct
 
+# if __name__=='__main__':
+    # read_sonyc_num_datasets('/scratch/dr2915/reduced_embeddings/sonyc_files_list.csv')
+    # check_sonyc_openl3_points('/beegfs/work/sonyc/features/openl3_day_format/2017',
+    #                           '/scratch/dr2915/reduced_embeddings/sonyc_files_list.csv',
+    #                           min_num_datasets_per_file=1400, max_num_datasets_per_file=1500)
