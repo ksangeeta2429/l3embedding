4d629c503a543d0757b0f7399aa38eaf25494ee6
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-04 13:24:53 -0400

    Increasing sleep to 5s

--- jobs/emb_approx_mse/umap_training/generate_trained_embedding/jobs_gen_trained_umap.sh
+++ jobs/emb_approx_mse/umap_training/generate_trained_embedding/jobs_gen_trained_umap.sh
@@ -24,42 +24,42 @@ sbatch gen_trained_umap_emb.sbatch valid umap_ndata=2048000_emb=256_nbrs=50_mind
 
 # MAHIN: Trains -- umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 0
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 1
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 2
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 3
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 4
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 5
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 6
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 7
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 8
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=40_mindist=0.3_mtrc=euclidean.sav 102400 9
 
 # CHARLIE: Trains -- umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 0
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 1
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 2
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 3
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 4
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 5
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 6
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 7
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 8
-sleep 1
+sleep 5
 sbatch gen_trained_umap_emb.sbatch train umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 9
