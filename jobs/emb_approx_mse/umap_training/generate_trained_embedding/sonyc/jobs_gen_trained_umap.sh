927c0614e0bafdb2c263b05e19b3708bde81661c
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-10 11:43:29 -0400

    Forgot Mahin's assignments

--- jobs/emb_approx_mse/umap_training/generate_trained_embedding/sonyc/jobs_gen_trained_umap.sh
+++ jobs/emb_approx_mse/umap_training/generate_trained_embedding/sonyc/jobs_gen_trained_umap.sh
@@ -64,3 +64,25 @@ sleep 5
 sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean.sav 102400 8
 sleep 5
 sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean.sav 102400 9
+
+
+# MAHIN: SONYC -- umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 0
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 1
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 2
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 3
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 4
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 5
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 6
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 7
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 8
+sleep 5
+sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400 9
