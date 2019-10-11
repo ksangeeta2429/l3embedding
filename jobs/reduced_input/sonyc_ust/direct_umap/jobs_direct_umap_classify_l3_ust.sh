0fcb52f9202bbadd66f7fed8d82d096cee240e58
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-10-11 15:29:26 -0400

    Jobs to classify on ust with direct umap

---
+++ jobs/reduced_input/sonyc_ust/direct_umap/jobs_direct_umap_classify_l3_ust.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean
