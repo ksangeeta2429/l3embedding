aa1b675a52b2cd26f73d12e36ee7ff0d66112e52
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-11 20:40:28 -0400

    Relocating gen

--- jobs/emb_approx_mse/umap_training/generate_trained_embedding/sonyc_ust/jobs_sonyc_ust_gen_trained_umap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=20_mindist=0.3_mtrc=euclidean.sav 102400
-sleep 5
-sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean.sav 102400
-sleep 5
-sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean.sav 102400
-sleep 5
-sbatch gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400
