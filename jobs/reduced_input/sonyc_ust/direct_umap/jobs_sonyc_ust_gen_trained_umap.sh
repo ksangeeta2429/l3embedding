01193f202af823f8bc6727d460ebbb1ecdcccfa3
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-14 22:55:17 -0400

    Minor fix

--- jobs/reduced_input/sonyc_ust/direct_umap/jobs_sonyc_ust_gen_trained_umap.sh
+++ jobs/reduced_input/sonyc_ust/direct_umap/jobs_sonyc_ust_gen_trained_umap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# UMAP trained on Music
+# UMAP trained on SONYC
 sbatch sonyc_ust_gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=20_mindist=0.3_mtrc=euclidean.sav 102400
 sleep 5
 sbatch sonyc_ust_gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean.sav 102400
@@ -9,6 +9,7 @@ sbatch sonyc_ust_gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/
 sleep 5
 sbatch sonyc_ust_gen_trained_umap_emb.sbatch /scratch/sk7898/reduced_embeddings/umap/models/sonyc/umap_ndata=2048000_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean.sav 102400
 
-# UMAP trained on SONYC
+# UMAP trained on music
 sbatch sonyc_ust_gen_trained_umap_emb.sbatch /scratch/dr2915/reduced_embeddings/umap/models/music/umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean.sav 102400
+sleep 5
 sbatch sonyc_ust_gen_trained_umap_emb.sbatch /scratch/dr2915/reduced_embeddings/umap/models/music/umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean.sav 102400
