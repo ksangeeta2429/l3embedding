63ce77f5c8b1a567b6251b9b1d37d7c09f55cf06
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-10-15 16:22:27 -0400

    Adding music umap jobs on sunyc_ust

--- jobs/reduced_input/sonyc_ust/direct_umap/jobs_direct_umap_classify_l3_ust.sh
+++ jobs/reduced_input/sonyc_ust/direct_umap/jobs_direct_umap_classify_l3_ust.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean
-sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean sonyc
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean sonyc
+
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean music
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean music
