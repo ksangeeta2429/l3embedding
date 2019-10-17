6eced91edb0856fdbb836cfbfad8b140ac64e182
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-17 00:24:34 -0400

    Adding mode 'BASELINE' to model_type

--- jobs/reduced_input/sonyc_ust/baselines/baselines_classify_l3_ust.sh
+++ jobs/reduced_input/sonyc_ust/baselines/baselines_classify_l3_ust.sh
@@ -2,37 +2,37 @@
 
 # With no hidden layer
 sbatch classify_l3_ust.sbatch 20190723135600_16000_128_128_512_half
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190723135620_8000_128_64_256_half_fmax_3500
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190710131207_16000_64_160_1024
 
 # With one hidden layer
 sbatch classify_l3_ust.sbatch 20190723135600_16000_128_128_512_half 1 128
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190723135620_8000_128_64_256_half_fmax_3500 1 128
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190710131207_16000_64_160_1024 1 128
-sleep 1
+sleep 3
 
 sbatch classify_l3_ust.sbatch 20190723135600_16000_128_128_512_half 1 256
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190723135620_8000_128_64_256_half_fmax_3500 1 256
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190710131207_16000_64_160_1024 1 256
-sleep 1
+sleep 3
 
 # With two hidden layers
 sbatch classify_l3_ust.sbatch 20190723135600_16000_128_128_512_half 2 128
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190723135620_8000_128_64_256_half_fmax_3500 2 128
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190710131207_16000_64_160_1024 2 128
-sleep 1
+sleep 3
 
 sbatch classify_l3_ust.sbatch 20190723135600_16000_128_128_512_half 2 256
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190723135620_8000_128_64_256_half_fmax_3500 2 256
-sleep 1
+sleep 3
 sbatch classify_l3_ust.sbatch 20190710131207_16000_64_160_1024 2 256
-sleep 1
+sleep 3
