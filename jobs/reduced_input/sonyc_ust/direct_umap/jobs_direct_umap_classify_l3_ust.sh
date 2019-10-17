586f3ec4ed7f36f19278c0818c1b02d3c08ab9c4
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-16 22:15:21 -0400

    Adding ust training with 1 hidden layer

--- jobs/reduced_input/sonyc_ust/direct_umap/jobs_direct_umap_classify_l3_ust.sh
+++ jobs/reduced_input/sonyc_ust/direct_umap/jobs_direct_umap_classify_l3_ust.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+# With no hidden layers
 sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean sonyc
 sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean sonyc
 
 sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean music
 sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean music
+
+# With one hidden layer
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean music 1 128
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean music 1 128
+
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=10_mindist=0.3_mtrc=euclidean music 1 256
+sbatch direct_umap_classify_l3_ust.sbatch umap_ndata=2048000_emb=256_nbrs=30_mindist=0.3_mtrc=euclidean music 1 256
