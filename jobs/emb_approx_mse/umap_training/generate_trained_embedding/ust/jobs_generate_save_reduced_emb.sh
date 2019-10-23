6daf7b7e6b1f7f9f28ac362d4d1ea984a8eeddb4
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-10-23 19:51:37 -0400

    Some mods

--- jobs/emb_approx_mse/umap_training/generate_trained_embedding/ust/jobs_generate_save_reduced_emb.sh
+++ jobs/emb_approx_mse/umap_training/generate_trained_embedding/ust/jobs_generate_save_reduced_emb.sh
@@ -1,8 +1,16 @@
 sbatch generate_save_reduced_emb.sbatch 50 0.3
+sleep 3
 sbatch generate_save_reduced_emb.sbatch 50 0.5
+sleep 3
 sbatch generate_save_reduced_emb.sbatch 100 0.3
+sleep 3
 sbatch generate_save_reduced_emb.sbatch 100 0.5
+sleep 3
 sbatch generate_save_reduced_emb.sbatch 200 0.3
+sleep 3
 sbatch generate_save_reduced_emb.sbatch 200 0.5
+sleep 3
 sbatch generate_save_reduced_emb.sbatch 300 0.3
+sleep 3
 sbatch generate_save_reduced_emb.sbatch 300 0.5
+sleep 3
