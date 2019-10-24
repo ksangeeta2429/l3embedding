244b0d7d206040d0c0aa73a407cce71df8952df3
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-24 14:34:37 -0400

    Adding 5M training points extraction on music

--- jobs/emb_approx_mse/umap_training/create_dataset/jobs_create_dataset.sh
+++ jobs/emb_approx_mse/umap_training/create_dataset/jobs_create_dataset.sh
@@ -2,4 +2,6 @@
 
 sbatch create_training_dataset_umap.sbatch music 2048000
 sleep 5
+sbatch create_training_dataset_umap.sbatch music 5120000
+sleep 5
 sbatch create_training_dataset_umap.sbatch sonyc 2048000
