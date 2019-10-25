fc42154b8fe5f51fab10608ac1fed0f7616e96b4
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-10-24 22:55:47 -0400

    Adding ust direct classification jobs

--- jobs/emb_approx_mse/umap_training/generate_trained_embedding/ust/gen_jobs_save_reduced_embedding.sh
+++ jobs/emb_approx_mse/umap_training/generate_trained_embedding/ust/gen_jobs_save_reduced_embedding.sh
@@ -3,8 +3,8 @@
 NEIGHBORS_LIST=( 50 100 200 300 )
 MIN_DIST_LIST=( 0.3 0.5 )
 
-for nbr in ${NEIGHBORS_LIST[@]}; do
-    for dst in ${MIN_DIST_LIST[@]}; do
+for nbr in "${NEIGHBORS_LIST[@]}"; do
+    for dst in "${MIN_DIST_LIST[@]}"; do
         echo sbatch generate_save_reduced_emb.sbatch $nbr $dst >> jobs_generate_save_reduced_emb.sh
         echo sleep 3 >> jobs_generate_save_reduced_emb.sh
     done
