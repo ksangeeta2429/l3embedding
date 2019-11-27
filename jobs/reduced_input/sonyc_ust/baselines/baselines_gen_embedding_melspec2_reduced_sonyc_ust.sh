06d27bcf371f95a2b2002f35afd3b26f79dddd79
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-11-27 00:33:29 -0500

    Adding ust embedding extraction for new student

--- jobs/reduced_input/sonyc_ust/baselines/baselines_gen_embedding_melspec2_reduced_sonyc_ust.sh
+++ jobs/reduced_input/sonyc_ust/baselines/baselines_gen_embedding_melspec2_reduced_sonyc_ust.sh
@@ -3,3 +3,7 @@ sleep 30
 sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20190723135620_8000_128_64_256_half_fmax_3500.h5 8000 128 64 256 3500
 sleep 30
 sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20190710131207_16000_64_160_1024.h5 16000 64 160 1024
+
+# After introducing auto-parsing of melspec params
+# Student that is <100% duty cycle
+sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20191108201753_8000_64_160_1024_half.h5
