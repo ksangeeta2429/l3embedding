6a386f2e2d45ac23c4186b62a6f7fdbe783c612d
Dhrubojyoti Roy <royd@cse.ohio-state.edu>
2019-12-10 17:21:17 -0500

    Finalizing direct umap jobs on ust

--- jobs/reduced_input/sonyc_ust/direct_umap/jobs_large_nbrs_generate_save_reduced_emb.sh
+++ jobs/reduced_input/sonyc_ust/direct_umap/jobs_large_nbrs_generate_save_reduced_emb.sh
@@ -1,13 +1,14 @@
 # Min_dist = 0.3
-sbatch generate_save_reduced_emb.sbatch 1000 0.3
+sbatch --mem=100GB generate_save_reduced_emb.sbatch 1000 0.3
 sleep 3
-sbatch generate_save_reduced_emb.sbatch 10000 0.3
+sbatch --mem=100GB generate_save_reduced_emb.sbatch 5000 0.3
 sleep 3
-sbatch generate_save_reduced_emb.sbatch 100000 0.3
+sbatch --mem=100GB generate_save_reduced_emb.sbatch 10000 0.3
+sleep 3
 
 #Min_dist = 0.5
-sbatch generate_save_reduced_emb.sbatch 1000 0.5
+sbatch --mem=100GB generate_save_reduced_emb.sbatch 1000 0.5
 sleep 3
-sbatch generate_save_reduced_emb.sbatch 10000 0.5
+sbatch --mem=100GB generate_save_reduced_emb.sbatch 5000 0.5
 sleep 3
-sbatch generate_save_reduced_emb.sbatch 100000 0.5
+sbatch --mem=100GB generate_save_reduced_emb.sbatch 10000 0.5
