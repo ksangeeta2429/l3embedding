c728438965b5bfdc6699fceaf8202d01d45f54ce
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-12-19 14:14:38 -0500

    Adding MLP jobs for 8K no BN after input model

---
+++ jobs/reduced_input/sonyc_ust/baselines/jobs_mlp_20191108201753_8000_64_160_1024_half_for_quantization_flattenOp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# With no hidden layer
+sbatch classify_mlp_l3_ust.sbatch 20191108201753_8000_64_160_1024_half_for_quantization_flattenOp
+sleep 3
+
+# With one hidden layer
+sbatch classify_mlp_l3_ust.sbatch 20191108201753_8000_64_160_1024_half_for_quantization_flattenOp 1 128
+sleep 3
+sbatch classify_mlp_l3_ust.sbatch 20191108201753_8000_64_160_1024_half_for_quantization_flattenOp 1 256
+sleep 3
+
+# With two hidden layers
+sbatch classify_mlp_l3_ust.sbatch 20191108201753_8000_64_160_1024_half_for_quantization_flattenOp 2 128
+sleep 3
+sbatch classify_mlp_l3_ust.sbatch 20191108201753_8000_64_160_1024_half_for_quantization_flattenOp 2 256
