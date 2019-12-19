e92b889992b4da8b2c3d518493cd2f8e52f83244
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-12-19 14:38:47 -0500

    Adding back accidentally deleted jobs

---
+++ jobs/reduced_input/sonyc_ust/baselines/baselines_gen_embedding_melspec2_reduced_sonyc_ust.sh
@@ -0,0 +1,12 @@
+sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20190723135600_16000_128_128_512_half.h5 16000 128 128 512
+sleep 30
+sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20190723135620_8000_128_64_256_half_fmax_3500.h5 8000 128 64 256 3500
+sleep 30
+sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20190710131207_16000_64_160_1024.h5 16000 64 160 1024
+
+# After introducing auto-parsing of melspec params
+# Student that is <100% duty cycle
+sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20191108201753_8000_64_160_1024_half.h5
+
+# Without BN layer after input
+sbatch gen_embedding_melspec2_reduced_sonyc_ust.sbatch l3_audio_20191108201753_8000_64_160_1024_half_for_quantization_flattenOp.h5 
