49cca71d4e073c617e686ebf086ce116ac2a880d
Dhrubojyoti Roy <royd@cse.osu.edu>
2020-01-01 11:19:41 -0500

    Fixing jobs, job names, etc

--- jobs/reduced_input/sonyc_ust/direct_umap/gen_jobs_classify.sh
+++ jobs/reduced_input/sonyc_ust/direct_umap/gen_jobs_classify.sh
@@ -2,19 +2,19 @@
 
 NEIGHBORS_LIST=( 25 50 75 )
 MIN_DIST_LIST=( 0.3 0.5 0.7 )
-
+OUTFILE=jobs_classify_umap_gpu.sh #jobs_direct_umap_classify_l3_ust.sh
 for nbr in "${NEIGHBORS_LIST[@]}"; do
     for dst in "${MIN_DIST_LIST[@]}"; do
-        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean >> jobs_direct_umap_classify_l3_ust.sh
-        echo sleep 3 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 1 128 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sleep 3 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 1 256 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sleep 3 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 2 128 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sleep 3 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 2 256 >> jobs_direct_umap_classify_l3_ust.sh
-        echo sleep 3 >> jobs_direct_umap_classify_l3_ust.sh
-        echo "\n" >> jobs_direct_umap_classify_l3_ust.sh
+        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean >> $OUTFILE
+        echo sleep 3 >> $OUTFILE
+        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 1 128 >> $OUTFILE
+        echo sleep 3 >> $OUTFILE
+        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 1 256 >> $OUTFILE
+        echo sleep 3 >> $OUTFILE
+        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 2 128 >> $OUTFILE
+        echo sleep 3 >> $OUTFILE
+        echo sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=${nbr}_mindist=${dst}_mtrc=euclidean 2 256 >> $OUTFILE
+        echo sleep 3 >> $OUTFILE
+        #echo "\n" >> $OUTFILE
     done
 done
---
+++ jobs/reduced_input/sonyc_ust/direct_umap/jobs_classify_umap_gpu.sh
@@ -0,0 +1,90 @@
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.3_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.3_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.3_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.3_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.3_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.5_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.5_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.5_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.5_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.5_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.7_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.7_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.7_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.7_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=25_mindist=0.7_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.3_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.5_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.5_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.5_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.5_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.5_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.7_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.7_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.7_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.7_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=50_mindist=0.7_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.3_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.3_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.3_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.3_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.3_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.5_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.5_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.5_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.5_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.5_mtrc=euclidean 2 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.7_mtrc=euclidean
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.7_mtrc=euclidean 1 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.7_mtrc=euclidean 1 256
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.7_mtrc=euclidean 2 128
+sleep 3
+sbatch direct_umap_classify_l3_ust.sbatch umap_emb=256_nbrs=75_mindist=0.7_mtrc=euclidean 2 256
+sleep 3
