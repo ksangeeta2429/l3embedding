1c28606ae35c7b388ef05f341e9f367f8ef3c163
Dhrubojyoti Roy <royd@cse.osu.edu>
2020-01-04 15:00:27 -0500

    Utility to combine l3 and MLP models

--- classifier/utils.py
+++ classifier/utils.py
@@ -4,12 +4,9 @@ import os
 
 def create_sonyc_pipeline_model(l3_model_path, mlp_model_path):
     l3 = load_model(l3_model_path)
-    mlp = load_model(mlp_model_path)
 
-    # Pop MLP's input layer
-    mlp.layers.pop(0)
-
-    # Connect l3 with MLP
+    # Connect l3 with MLP (both functional models)
+    return Model(input=l3.input, output=load_model(l3_model_path)(l3.output))
 
 
 def convert_keras_classifier_to_TFLite(keras_model_path, out_path_name):
