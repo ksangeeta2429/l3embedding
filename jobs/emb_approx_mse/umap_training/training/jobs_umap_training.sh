ddb0f96db5027b5e1af5f3e84971f302db05648f
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2020-01-06 18:08:29 -0500

    Adding back accidentally deleted jobs

---
+++ jobs/emb_approx_mse/umap_training/training/jobs_umap_training.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#sbatch umap_training.sbatch music 1000000 256 5 0.3 correlation
+#sbatch umap_training.sbatch music 1500000 256 5 0.3 correlation
+#
+#sbatch big_batch_umap_training.sbatch music 5000000 256 5 0.3 correlation
+#sbatch big_batch_umap_training.sbatch music 10000000 256 5 0.3 correlation
+
+# Euclidean -- music dataset
+# sbatch umap_training.sbatch music 2048000 256 5 0.3 -- Error: numpy.linalg.linalg.LinAlgError: 173-th leading minor not positive definite
+#sbatch umap_training.sbatch music 5120000 256 1000 0.3
+#sbatch umap_training.sbatch music 5120000 256 2000 0.3
+sbatch umap_training.sbatch music 2048000 256 100000 0.3
+sbatch umap_training.sbatch music 1024000 256 100000 0.3
+#sleep 10
+#sbatch umap_training.sbatch music 2048000 256 300 0.3
+#sbatch umap_training.sbatch music 2048000 256 50 0.3
+
+# Euclidean -- sonyc dataset
+#sbatch umap_training.sbatch sonyc 2048000 256 10 0.3
+#sbatch umap_training.sbatch sonyc 2048000 256 20 0.3
+#sbatch umap_training.sbatch sonyc 2048000 256 30 0.3
+#sbatch umap_training.sbatch sonyc 2048000 256 40 0.3
+#sbatch umap_training.sbatch sonyc 2048000 256 50 0.3
