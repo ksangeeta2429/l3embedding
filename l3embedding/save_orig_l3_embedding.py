56a9a6885ee715dde6ddd2a61b6c90860cf0ead7
ksangeeta2429 <kumari.14@osu.edu>
2020-01-06 17:53:52 -0500

    new save_orig_emb

--- l3embedding/save_orig_l3_embedding.py
+++ l3embedding/save_orig_l3_embedding.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 import os
 import random
 import shutil
@@ -8,25 +11,45 @@ import tempfile
 import tensorflow as tf
 import multiprocessing
 from kapre.time_frequency import Melspectrogram
+from keras.layers import concatenate, Dense
 import keras
 from keras.layers import Activation
 from keras.models import Model
+from l3embedding.vision_model import *
 from skimage import img_as_float
 from keras import activations
 from l3embedding.audio import pcm2float
 from keras import backend as K
 from functools import partial
 
-
 # Do not allocate all the memory for visible GPU
-#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-#os.environ["CUDA_VISIBLE_DEVICES"]="-1"
-#config = tf.ConfigProto()
-#config.gpu_options.allow_growth=True
-#sess = tf.Session(config=config)
-#K.set_session(sess)
+sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True)))
+K.set_session(sess)
 
-def apply_modifications(model, custom_objects=None):
+def L3_merged_model(model, embedding_length=512, layer_size=128):
+    
+    # Merge the subnetworks
+    weight_decay = 1e-5
+    
+    vision_model, x_i, y_i = construct_cnn_L3_orig_inputbn_vision_model()
+    x_a = Input(shape=(embedding_length,), dtype='float32')
+    
+    y = concatenate([vision_model(x_i), x_a])
+    y = Dense(layer_size, activation='relu',
+              kernel_initializer='he_normal',
+              kernel_regularizer=regularizers.l2(weight_decay))(y)
+    y = Dense(2, activation='softmax',
+              kernel_initializer='he_normal',
+              kernel_regularizer=regularizers.l2(weight_decay))(y)
+    m = Model(inputs=[x_i, x_a], outputs=y)
+    
+    m.get_layer('vision_model').set_weights(model.get_layer('vision_model').get_weights())
+    m.layers[-1].set_weights(model.layers[-1].get_weights())
+    m.layers[-2].set_weights(model.layers[-2].get_weights())
+
+    return m, [x_i, x_a], y
+
+def apply_temp_modifications(model, custom_objects=None):
     """Applies modifications to the model layers to create a new Graph. For example, simply changing
     `model.layers[idx].activation= new activation` does not change the graph. The entire graph needs to be updated
     with modified inbound and outbound tensors because of change in layer building function.
@@ -45,52 +68,33 @@ def apply_modifications(model, custom_objects=None):
         return keras.models.load_model(model_path, custom_objects=custom_objects)
     finally:
         os.remove(model_path)
-
-def write_to_h5(path, batch):
-    with h5py.File(path, 'a') as f:
-        for key in batch.keys():
-            if key in f.keys():
-                continue
-            f.create_dataset(key, data=batch[key], compression='gzip')
-        f.close()
+        
+def save_logits_model(model, out_path, custom_objects=None):
+    model.save(out_path)
+    return keras.models.load_model(out_path, custom_objects=custom_objects)
 
 def get_teacher_logits(model, video_batch, audio_batch):
     if model is None:
         raise ValueError('Teacher L3 not provided!')
-    
-    test = False
-    softmax = model.predict([video_batch, audio_batch])
-    model.layers[-1].activation = activations.linear
-    model = apply_modifications(model, custom_objects={'Melspectrogram': Melspectrogram})
+
     logits = model.layers[-1].output
     
     model_WO_softmax = Model(model.input, logits)
     predicted_logits = model_WO_softmax.predict([video_batch, audio_batch])
 
-    if test:
-        probabilities = Activation('softmax')(logits)
-        verification_model = Model(model.input, probabilities)
-        softmax_test = verification_model.predict([video_batch, audio_batch])
-        print(softmax[:10])
-        print(predicted_logits[:10])
-        print(softmax_test[:10])
-        exit(0)
+    return predicted_logits
 
-    return predicted_logits, softmax
+def get_teacher_softmax(model, video_batch, audio_batch):
+    softmax = model.predict([video_batch, audio_batch])
+    return softmax 
 
-def run_in_parallel(iterable, function, processes=8):
-    pool = multiprocessing.Pool(processes=processes)
-    output = pool.map(function, iterable)
-    return list(output)
-
-def freeze(model):
-    """Freeze model weights in every layer."""
-    for layer in model.layers:
-        layer.trainable = False
-
-        if isinstance(layer, keras.models.Model):
-            freeze(layer)
-
+def write_to_h5(f, batch):
+    for key in f.keys():
+        if key in f.keys():
+            continue
+        f.create_dataset(key, data=batch[key], compression='gzip')
+    f.close()
+    
 def generate_output_driver(data_dir, output_dir, out_type='l3_embedding', partition_to_run=None,\
                            num_partitions=10, start_idx=None, **kwargs):
 
@@ -101,110 +105,22 @@ def generate_output_driver(data_dir, output_dir, out_type='l3_embedding', partit
 
     all_files = os.listdir(data_dir)
     new_dir = None
-    copy = False
-    cpu = False
 
     if 'valid' in output_dir:
         new_dir = '/scratch/sk7898/orig_l3_embeddings/music_valid_new'
     elif 'train' in output_dir:
-        new_dir = '/scratch/sk7898/orig_l3_embeddings/music_train_new'
+        new_dir = '/scratch/sk7898/orig_l3_embeddings/music_train_logits'
 
-    if copy:
-        for fname in all_files:
-            out_path = os.path.join(output_dir, fname)
-            if os.path.exists(out_path) and out_type in h5py.File(out_path,'r').keys():
-                shutil.move(os.path.join(output_dir, fname), new_dir)
-                continue
+    remaining_files = os.listdir(output_dir)
+    all_files = list(divide_chunks(remaining_files, math.ceil(len(remaining_files) / num_partitions)))
 
-    num_files = len(all_files)
-    #remaining_files = all_files[start_idx: num_files]
-    remaining_files = list(set(os.listdir(data_dir)) - set(os.listdir(new_dir)))
-    all_files = list(divide_chunks(all_files, math.ceil(num_files / num_partitions)))
-
-    # Get list of files to run
-    # print('#Processed files: {}'.format(idx+1))
     print('Partition to run: {} out of {} partitions'.format(partition_to_run, num_partitions))
     list_files = all_files[partition_to_run]
 
-    if cpu:
-        print('Total Workers: ',multiprocessing.cpu_count())
-        worker_func = partial(embedding_generator_cpu, data_dir=data_dir, output_dir=output_dir, out_type=out_type, **kwargs)
-        run_in_parallel(list_files, worker_func, processes=multiprocessing.cpu_count())
-    else:
-        embedding_generator(data_dir=data_dir, output_dir=output_dir, out_type=out_type, list_files=list_files, **kwargs)
+    embedding_generator(data_dir=data_dir, new_dir=new_dir, output_dir=output_dir, out_type=out_type, list_files=list_files, **kwargs)
 
 
-def embedding_generator_cpu(list_files, data_dir, output_dir, out_type='l3_embedding', batch_size=256, **kwargs):
-
-    if data_dir == output_dir:
-        raise ValueError('Output path should not be same as data path to avoid overwriting data files!')
-
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
-
-    if list_files == None:
-        list_files = os.listdir(data_dir)
-
-    weight_path = '/scratch/sk7898/l3pruning/embedding/fixed/reduced_input/l3_full_original_48000_256_242_2048.h5'
-    model = keras.models.load_model(weight_path, custom_objects={'Melspectrogram': Melspectrogram})
-
-    idx = 0
-    new_dir = None
-
-    if 'valid' in output_dir:
-        new_dir = '/scratch/sk7898/orig_l3_embeddings/music_valid_new'
-    elif 'train' in output_dir:
-        new_dir = '/scratch/sk7898/orig_l3_embeddings/music_train_new'
-
-    for fname in list_files:
-        blob_start_idx = 0
-        out_blob = None
-
-        out_path = os.path.join(output_dir, fname)
-        if os.path.exists(out_path) and out_type in h5py.File(out_path,'r').keys():
-            idx += 1
-            continue
-
-        batch_path = os.path.join(data_dir, fname)
-        blob = h5py.File(batch_path, 'r')
-
-        if out_type == 'embedding':
-            batch = {'audio': blob['audio']}
-            batch['audio'] = pcm2float(batch['audio'], dtype='float32')
-            audio_model = model.get_layer('audio_model')
-            out_blob['l3_embedding'] = audio_model.predict(batch['audio'])
-
-        elif out_type == 'logits':
-            batch = {'audio': blob['audio'], 'video': blob['video']}
-            batch['audio'] = pcm2float(batch['audio'], dtype='float32')
-            batch['video'] = 2 * img_as_float(batch['video']).astype('float32') - 1
-
-            blob_size = len(batch['audio'])
-
-            while blob_start_idx < blob_size:
-                blob_end_idx = min(blob_start_idx + batch_size, blob_size)
-                audio_batch = batch['audio'][blob_start_idx:blob_end_idx]
-                video_batch = batch['video'][blob_start_idx:blob_end_idx]
-
-                logits_out, softmax_out = get_teacher_logits(model, video_batch, audio_batch)
-                if out_blob is None:
-                    out_blob = {'logits': logits_out, 'softmax': softmax_out}
-                else:
-                    out_blob['logits'] = np.concatenate([out_blob['logits'], logits_out])
-                    out_blob['softmax'] = np.concatenate([out_blob['softmax'], softmax_out])
-
-                blob_start_idx = blob_end_idx
-        else:
-            raise ValueError('Output type is not supported!')
-
-        write_to_h5(out_path, out_blob)
-        shutil.move(out_path, new_dir)
-
-        idx += 1
-        print('File {}: {} done!'.format(idx, fname))
-        blob.close()
-
-def embedding_generator(data_dir, output_dir, out_type='l3_embedding', list_files=None, batch_size=256, **kwargs):
+def embedding_generator(data_dir, output_dir, new_dir=None, out_type='l3_embedding', list_files=None, batch_size=64, **kwargs):
     
     if data_dir == output_dir:
         raise ValueError('Output path should not be same as data path to avoid overwriting data files!')
@@ -217,30 +133,39 @@ def embedding_generator(data_dir, output_dir, out_type='l3_embedding', list_file
 
     weight_path = '/scratch/sk7898/l3pruning/embedding/fixed/reduced_input/l3_full_original_48000_256_242_2048.h5'
     model = keras.models.load_model(weight_path, custom_objects={'Melspectrogram': Melspectrogram}) 
+    
+    if out_type == 'logits' or out_type == 'softmax':
+        out_path = '/scratch/sk7898/l3pruning/embedding/fixed/reduced_input/l3_full_original_48000_256_242_2048_logits.h5'
+        model, inputs, outputs = L3_merged_model(model)
+        #print(model.summary()) 
+        
+        if os.path.exists(out_path):
+            logits_model = keras.models.load_model(out_path)
+        else:
+            model.layers[-1].activation = activations.linear
+            logits_model = save_logits_model(model, out_path, custom_objects={'Melspectrogram': Melspectrogram})
 
     idx = 0
-    new_dir = None
-
-    if 'valid' in output_dir:
-        new_dir = '/scratch/sk7898/orig_l3_embeddings/music_valid_new'
-    elif 'train' in output_dir:
-        new_dir = '/scratch/sk7898/orig_l3_embeddings/music_train_new'
 
     for fname in list_files:
         blob_start_idx = 0
         out_blob = None
                 
         out_path = os.path.join(output_dir, fname)
-        if os.path.exists(out_path) and out_type in h5py.File(out_path,'r').keys():
-            idx += 1
-<<<<<<< Updated upstream
-=======
-            print('Skipping file {}: {}! Already exists!'.format(idx, fname))
->>>>>>> Stashed changes
-            continue
+        new_path = os.path.join(new_dir, fname)
+        if os.path.exists(new_path):
+            if out_type in h5py.File(new_path,'r').keys() and 'l3_embedding' in h5py.File(new_path,'r').keys():
+                idx += 1
+                continue
+            elif os.path.exists(out_path) and 'l3_embedding' in h5py.File(out_path,'r').keys():
+                os.remove(new_path)
+            else:
+                print('Some corruption in file:', new_path)
+                continue
 
         batch_path = os.path.join(data_dir, fname)
         blob = h5py.File(batch_path, 'r')
+        emb_blob = h5py.File(out_path, 'a')
         
         if out_type == 'embedding':
             batch = {'audio': blob['audio']}
@@ -249,41 +174,47 @@ def embedding_generator(data_dir, output_dir, out_type='l3_embedding', list_file
             out_blob['l3_embedding'] = audio_model.predict(batch['audio'])
 
         elif out_type == 'logits':
-            batch = {'audio': blob['audio'], 'video': blob['video']}
-            batch['audio'] = pcm2float(batch['audio'], dtype='float32')
-            batch['video'] = 2 * img_as_float(batch['video']).astype('float32') - 1
-            
-            blob_size = len(batch['audio'])
+            batch = {'audio_emb': emb_blob['l3_embedding'], 'video': blob['video']}
+            batch['video'] = 2 * img_as_float(batch['video']).astype('float32') - 1            
+            blob_size = len(batch['video'])
             
             while blob_start_idx < blob_size:
                 blob_end_idx = min(blob_start_idx + batch_size, blob_size)
-                audio_batch = batch['audio'][blob_start_idx:blob_end_idx]
+                audio_batch = batch['audio_emb'][blob_start_idx:blob_end_idx]
                 video_batch = batch['video'][blob_start_idx:blob_end_idx]
 
-                logits_out, softmax_out = get_teacher_logits(model, video_batch, audio_batch)
-<<<<<<< Updated upstream
+                logits_out = get_teacher_logits(logits_model, video_batch, audio_batch)
                 if out_blob is None:
-                    out_blob = {'logits': logits_out, 'softmax': softmax_out}
+                    out_blob = {'logits': logits_out}
                 else:
                     out_blob['logits'] = np.concatenate([out_blob['logits'], logits_out])
-                    out_blob['softmax'] = np.concatenate([out_blob['softmax'], softmax_out])
-                 
-=======
-                if 'logits' in out_blob.keys():
-                    out_blob['logits'] = np.concatenate([out_blob['logits'], logits_out])
-                    out_blob['softmax'] = np.concatenate([out_blob['softmax'], softmax_out])
-                else:
-                    out_blob = {'logits': logits_out, 'softmax': softmax_out}
 
-                curr_size += blob_end_idx 
->>>>>>> Stashed changes
                 blob_start_idx = blob_end_idx
+
+        elif out_type == 'softmax':
+            batch = {'audio_emb': emb_blob['l3_embedding'], 'video': blob['video']}
+            batch['video'] = 2 * img_as_float(batch['video']).astype('float32') - 1
+            
+            blob_size = len(batch['video'])
+            
+            while blob_start_idx < blob_size:
+                blob_end_idx = min(blob_start_idx + batch_size, blob_size)
+                audio_batch = batch['audio_emb'][blob_start_idx:blob_end_idx]
+                video_batch = batch['video'][blob_start_idx:blob_end_idx]
+
+                softmax_out = get_teacher_softmax(model, video_batch, audio_batch)
+                if out_blob is None:
+                    out_blob = {'softmax': softmax_out}
+                else:
+                    out_blob['softmax'] = np.concatenate([out_blob['softmax'], softmax_out])
+                    
         else:
             raise ValueError('Output type is not supported!')
 
-        write_to_h5(out_path, out_blob)
+        write_to_h5(emb_blob, out_blob)
         shutil.move(out_path, new_dir) 
     
         idx += 1
         print('File {}: {} done!'.format(idx, fname))
         blob.close()
+        emb_blob.close()
